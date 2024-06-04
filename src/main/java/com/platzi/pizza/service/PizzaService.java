8fd405ff5ee80a5511e4a5b31fbe4b04ce3c843a
Manuel Moreno <jmmoreno@rotoplas.com>
2024-06-04 17:09:03 -0600

    ADD: Operaciones CRUD con ListCrudRepository

--- src/main/java/com/platzi/pizza/service/PizzaService.java
+++ src/main/java/com/platzi/pizza/service/PizzaService.java
@@ -1,24 +1,38 @@
 package com.platzi.pizza.service;
 
 import com.platzi.pizza.persitence.entity.PizzaEntity;
+import com.platzi.pizza.persitence.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class PizzaService {
-    private final JdbcTemplate jdbcTemplate;
+    private final PizzaRepository pizzaRepository;
 
     @Autowired
-    public PizzaService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public PizzaService(PizzaRepository pizzaRepository) {
+        this.pizzaRepository = pizzaRepository;
     }
 
     public List<PizzaEntity> getAll() {
-        return this.jdbcTemplate.query("select * from pizza where available=0", new BeanPropertyRowMapper<>(PizzaEntity.class));
+        return this.pizzaRepository.findAll();
+    }
 
+    public PizzaEntity get(int idPizza) {
+        return this.pizzaRepository.findById(idPizza).orElse(null);
+    }
+
+    public PizzaEntity save(PizzaEntity pizza) {
+        return this.pizzaRepository.save(pizza);
+    }
+
+    public boolean exists(int idPizza) {
+        return this.pizzaRepository.existsById(idPizza);
+    }
+
+    public void delete(int idPizza) {
+        this.pizzaRepository.deleteById(idPizza);
     }
 }
--- src/main/java/com/platzi/pizza/web/controller/PizzaController.java
+++ src/main/java/com/platzi/pizza/web/controller/PizzaController.java
@@ -2,16 +2,16 @@ package com.platzi.pizza.web.controller;
 
 import com.platzi.pizza.persitence.entity.PizzaEntity;
 import com.platzi.pizza.service.PizzaService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/pizzas")
+@Slf4j
 public class PizzaController {
     private final PizzaService pizzaService;
 
@@ -23,5 +23,42 @@ public class PizzaController {
     @GetMapping
     public ResponseEntity<List<PizzaEntity>> getAllPizzas() {
         return ResponseEntity.ok(this.pizzaService.getAll());
+    }
+
+    @GetMapping("/{idPizza}")
+    public ResponseEntity<PizzaEntity> get(@PathVariable int idPizza) {
+        if (this.pizzaService.exists(idPizza)) {
+            return ResponseEntity.ok(this.pizzaService.get(idPizza));
+        } else {
+            return ResponseEntity.noContent().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<PizzaEntity> add(@RequestBody PizzaEntity pizza) {
+        if (pizza.getIdPizza() == null || !this.pizzaService.exists(pizza.getIdPizza())) {
+            return ResponseEntity.ok(this.pizzaService.save(pizza));
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<PizzaEntity> update(@RequestBody PizzaEntity pizza) {
+        if (pizza.getIdPizza() != null && this.pizzaService.exists(pizza.getIdPizza())) {
+            return ResponseEntity.ok(this.pizzaService.save(pizza));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @DeleteMapping("/{idPizza}")
+    public ResponseEntity<Void> delete(@PathVariable int idPizza) {
+        if (this.pizzaService.exists(idPizza)) {
+            this.pizzaService.delete(idPizza);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+
     }
 }
